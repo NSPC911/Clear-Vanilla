e065706bd012e37cdc041f5b715f7a13c1bbd18e
NSPC911 <notsoprocoder911@gmail.com>
2024-04-14 08:05:44 +0800

    Added new keys for waxed blocks. More texturing incoming!

--- zipper.py
+++ zipper.py
@@ -4,8 +4,7 @@ import time
 
 output_dir = '.'
 repo_path = os.getcwd()
-repo_name = os.path.basename(repo_path)
-output_path = os.path.join(output_dir, f"{repo_name}.mcpack")
+output_path = os.path.join(output_dir, f"pack.mcpack")
 start_time = time.time()
 with zipfile.ZipFile(output_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
     for root, dirs, files in os.walk(repo_path):
@@ -15,14 +14,17 @@ with zipfile.ZipFile(output_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
             files.remove('.gitattributes')
         if 'zipper.py' in files:
             files.remove('zipper.py')
-        if f"{repo_name}.mcpack" in files:
-            files.remove(f"{repo_name}.mcpack")
+        if "pack.mcpack" in files:
+            files.remove(f"pack.mcpack")
         if os.path.abspath(__file__) in files:
             files.remove(os.path.abspath(__file__))
         for file in files:
             file_path = os.path.join(root, file)
             file_path_length = len(file_path)
-            print(f"\rArchiving: {file_path}{' ' * (os.get_terminal_size().columns - file_path_length - 12)}", end='', flush=True)
+            try:
+                print(f"\rArchiving: {file_path}{' ' * (os.get_terminal_size().columns - file_path_length - 12)}", end='', flush=True)
+            except OSError:
+                print(f"\rArchiving: {file_path}", end='', flush=True)
             zipf.write(file_path, os.path.relpath(file_path, repo_path))
 elapsed_time = time.time() - start_time
 print(f"\nRepository zipped successfully at {output_path}")
